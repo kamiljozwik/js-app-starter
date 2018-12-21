7ba0a87a66fa9afb0f9497d2da3c208f0c67036d
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2018-12-21 13:49:41 +0100

    js-app-starter

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Cześć!</h1>
+<div>
+  To jest startowa aplikacja js przygotowana przez <a href="https://www.frontstack.pl" target="_blank" rel="noopener noreferrer">frontstack.pl</a>.
+</div>
+<span>Happy coding!!!</span>
+`;
